7eca4de306e388b956e1bac70bd093d2af189bee
Shohei YOSHIDA <syohex@gmail.com>
2021-12-05 21:49:01 +0900

    Add 2020 Day03 part2

--- advent_of_code/2020/day03/main.py
+++ advent_of_code/2020/day03/main.py
@@ -18,7 +18,7 @@ class Solver:
         self.rows = len(self.map_data)
         self.cols = len(self.map_data[0])
 
-    def part1(self) -> int:
+    def _count_tree(self, row_step: int, col_step: int) -> int:
         row = 0
         col = 0
         ret = 0
@@ -27,8 +27,19 @@ class Solver:
             if self.map_data[row][col] == '#':
                 ret += 1
 
-            row += 1
-            col = (col + 3) % self.cols
+            row += row_step
+            col = (col + col_step) % self.cols
+
+        return ret
+
+    def part1(self) -> int:
+        return self._count_tree(1, 3)
+
+    def part2(self) -> int:
+        ret = 1
+        steps = [(1, 1), (1, 3), (1, 5), (1, 7), (2, 1)]
+        for (row_step, col_step) in steps:
+            ret *= self._count_tree(row_step, col_step)
 
         return ret
 
@@ -50,6 +61,7 @@ def test() -> None:
 
     s = Solver(data)
     assert s.part1() == 7
+    assert s.part2() == 336
 
 
 def main() -> None:
@@ -60,8 +72,12 @@ def main() -> None:
 
     s = Solver(data)
     part1 = s.part1()
+    part2 = s.part2()
     assert part1 == 299
+    assert part2 == 3621285278
+
     print(f"part1: {part1}")
+    print(f"part2: {part2}")
 
 
 if __name__ == '__main__':
